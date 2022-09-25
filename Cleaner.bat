45deec8e8f3f8095972d940f21c72737e1b5f74e
Fnoberz <SarnaxOfficial@protonmail.com>
2022-09-25 07:15:53 +0700

    Update Cleaner.bat

--- Cleaner.bat
+++ Cleaner.bat
@@ -50,3 +50,53 @@ if errorlevel 2 goto :EOF
 
 %CurrentDir%mapper.exe spoofer.sys
 %CurrentDir%mapper.exe spoofer.sys
+
+:CACHE
+cls
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\Browser"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\db"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\dunno"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\priv"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\servers"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\subprocess"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\unconfirmed"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\authbrowser"
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\cache\crashometry"
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\cache\launcher_skip"
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\cache\launcher_skip_mtl2"
+cls
+echo.
+echo.
+echo                                                             CLEANED !!
+timeout 2 >nul
+goto CLEAN
+
+:CRASH
+cls
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\crashes\*.*"
+cls
+echo.
+echo.
+echo                                                             CLEANED !!
+timeout 2 >nul
+goto CLEAN
+
+:LOGS
+cls
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\logs\*.*"
+cls
+echo.
+echo.
+echo                                                             CLEANED !!
+timeout 2 >nul
+goto CLEAN
+
+:MODS
+cls
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\mods\*.*"
+cls
+echo.
+echo.
+echo                                                             CLEANED !!
+timeout 2 >nul
+goto CLEAN
