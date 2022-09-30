62a2ec5377f9f24e18b5ed9b560ba50f5b08e25a
Fnoberz <FnoberzOfficial@pm.me>
2022-10-01 06:06:22 +0700

    Add files via upload

---
+++ Taskbar.Kill/Taskkill_clean.bat
@@ -0,0 +1,27 @@
+@echo off
+taskkill /f /im EasyAntiCheat_Setup.exe
+taskkill /f /im FortniteLauncher.exe
+taskkill /f /im EpicWebHelper.exe
+taskkill /f /im FortniteClient-Win64-Shipping.exe
+taskkill /f /im EasyAntiCheat.exe
+taskkill /f /im BEService_x64.exe
+taskkill /f /im EpicGamesLauncher.exe
+taskkill /f /im FortniteClient-Win64-Shipping_BE.exe
+taskkill /f /im FortniteClient-Win64-Shipping_EAC.exe
+sc stop BEService
+sc stop EasyAntiCheat
+:internettest
+cls
+echo waiting for internet reconnection
+echo you may need to manually reconnect to wifi
+echo if no wifi networks exist or not reconnecting
+echo go to device manager then uninstall your network card
+echo its under the network adapters section
+echo it will probably have some kind of brand name
+echo then go to the action section and click "scan for hardware changes"
+echo then you will have internet
+set target=www.google.com
+ping %target% -n 1 | find "Reply" > nul
+if errorlevel==1 goto internettest
+cls
+
