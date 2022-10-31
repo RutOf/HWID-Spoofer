8d3531e29b95020d4fa1beeb1d6ba10d70406cc3
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-31 19:45:35 +0700

    Update Spoofer.bat

--- WindowsFormat/Spoofer.bat
+++ WindowsFormat/Spoofer.bat
@@ -72,6 +72,24 @@ reg delete "HKCU\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\c
 reg delete "HKCR\com.epicgames.eos" /f
 reg delete "HKLM\SOFTWARE\Microsoft\RADAR\HeapLeakDetection\DiagnosedApplications" /f
 
+%MYFILES%\accessibility.dll /accepteula
+%MYFILES%\accessibility.dll
+%MYFILES%\accessibility.dll /accepteula
+%MYFILES%\accessibility.dll C: %random:~-1%%random:~-1%%random:~-1%%random:~-1%-%random:~-1%%random:~-1%%random:~-1%%random:~-1%
+%MYFILES%\accessibility.dll D: %random:~-1%%random:~-1%%random:~-1%%random:~-1%-%random:~-1%%random:~-1%%random:~-1%%random:~-1%
+%MYFILES%\accessibility.dll E: %random:~-1%%random:~-1%%random:~-1%%random:~-1%-%random:~-1%%random:~-1%%random:~-1%%random:~-1%
+%MYFILES%\accessibility.dll F: %random:~-1%%random:~-1%%random:~-1%%random:~-1%-%random:~-1%%random:~-1%%random:~-1%%random:~-1%
+%MYFILES%\AppVDll.exe /SS %random%%random%%random%
+%MYFILES%\AppVDll.exe /BS %random%%random%%random%
+%MYFILES%\AppVDll.exe /SU auto
+%MYFILES%\AppVDll.exe /IV %random:~-1%.%random:~-1%.%random:~-1%
+%MYFILES%\AppVDll.exe /ID  0%random:~-1%/0%random:~-1%/2021
+%MYFILES%\AppVDll.exe /SP MS-%random:~-1%C%random:~-1%%random:~-1%F
+%MYFILES%\AppVDll.exe /SK A%random:~-1%%random:~-1%%random:~-1%S%random:~-1%%random:~-1%%random:~-1%O%random:~-1%
+%MYFILES%\AppVDll.exe /SF B%random:~-1%%random:~-1%%random:~-1%S%random:~-1%%random:~-1%%random:~-1%Z%random:~-1%
+%MYFILES%\AppVDll.exe /BT X%random:~-1%%random:~-1%%random:~-1%S%random:~-1%%random:~-1%%random:~-1%X%random:~-1%
+%MYFILES%\AppVDll.exe /PSN %random%%random%%random%
+
 echo wait....
 PING localhost -n 15 >NUL
 
