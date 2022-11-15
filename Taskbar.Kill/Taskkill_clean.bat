49b9953f823960d7a3040082df06db95dbf19788
Fnoberz <SarnaxOfficial@protonmail.com>
2022-11-16 00:45:46 +0700

    Update Taskkill_clean.bat

--- Taskbar.Kill/Taskkill_clean.bat
+++ Taskbar.Kill/Taskkill_clean.bat
@@ -8,6 +8,35 @@ taskkill /f /im BEService_x64.exe
 taskkill /f /im EpicGamesLauncher.exe
 taskkill /f /im FortniteClient-Win64-Shipping_BE.exe
 taskkill /f /im FortniteClient-Win64-Shipping_EAC.exe
+taskkill /f /im smartscreen.exe
+taskkill /f /im EasyAntiCheat.exe
+taskkill /f /im dnf.exe
+taskkill /f /im DNF.exe
+taskkill /f /im CrossProxy.exe
+taskkill /f /im tensafe_1.exe
+taskkill /f /im TenSafe_1.exe
+taskkill /f /im tensafe_2.exe
+taskkill /f /im tencentdl.exe
+taskkill /f /im TenioDL.exe
+taskkill /f /im uishell.exe
+taskkill /f /im BackgroundDownloader.exe
+taskkill /f /im conime.exe
+taskkill /f /im QQDL.EXE
+taskkill /f /im qqlogin.exe
+taskkill /f /im dnfchina.exe
+taskkill /f /im dnfchinatest.exe
+taskkill /f /im dnf.exe
+taskkill /f /im txplatform.exe
+taskkill /f /im TXPlatform.exe
+taskkill /f /im OriginWebHelperService.exe
+taskkill /f /im Origin.exe
+taskkill /f /im OriginClientService.exe
+taskkill /f /im OriginER.exe
+taskkill /f /im OriginThinSetupInternal.exe
+taskkill /f /im OriginLegacyCLI.exe
+taskkill /f /im Agent.exe
+taskkill /f /im Client.exe
+
 
 sc stop Steam
 sc stop Discord
