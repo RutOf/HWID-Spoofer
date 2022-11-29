d434fddab71ab602afe6f00c8222fe47f72b30c0
Fnoberz <SarnaxOfficial@protonmail.com>
2022-11-29 19:34:13 +0700

    Update Taskkill_clean.bat

--- Taskbar.Kill/Taskkill_clean.bat
+++ Taskbar.Kill/Taskkill_clean.bat
@@ -1,4 +1,15 @@
 @echo off
+
+pause
+
+taskkill /f /im epicgameslauncher.exe
+taskkill /f /im FortniteClient-Win64-Shipping_EAC.exe
+taskkill /f /im FortniteClient-Win64-Shipping.exe
+taskkill /f /im FortniteClient-Win64-Shipping_BE.exe
+taskkill /f /im FortniteLauncher.exe
+taskkill /f /im EpicGamesLauncher.exe
+taskkill /f /im FortniteClient-Win64-Shipping.exe
+taskkill /f /im EpicGamesLauncher.exe
 taskkill /f /im EasyAntiCheat_Setup.exe
 taskkill /f /im FortniteLauncher.exe
 taskkill /f /im EpicWebHelper.exe
