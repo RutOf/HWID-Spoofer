78f8aea61813baf673d3f62d136f24af10a085b6
Fnoberz <SarnaxOfficial@protonmail.com>
2022-12-01 00:03:51 +0700

    Update Spoofer.bat

--- WindowsFormat/Spoofer.bat
+++ WindowsFormat/Spoofer.bat
@@ -90,44 +90,6 @@ reg delete "HKLM\SOFTWARE\Microsoft\RADAR\HeapLeakDetection\DiagnosedApplication
 %MYFILES%\AppVDll.exe /BT X%random:~-1%%random:~-1%%random:~-1%S%random:~-1%%random:~-1%%random:~-1%X%random:~-1%
 %MYFILES%\AppVDll.exe /PSN %random%%random%%random%
 
-echo wait....
-PING localhost -n 15 >NUL
-exit 
-exit
-
-
-
-
-@echo off
-echo your shitty BaseBoard
-wmic baseboard get serialnumber
-echo your shitty Bios
-wmic bios get serialnumber
-echo your shitty Cpu
-wmic cpu get serialnumber
-echo your shitty DiskDrive (#1) C:
-wmic diskdrive get serialnumber
-echo Diskdrive (#2)
-wmic path win32_physicalmedia get SerialNumber
-echo Diskdrive (#3)
-wmic path win32_diskdrive get SerialNumber
-echo your shitty BaseBoard
-wmic baseboard get manufacturer
-echo RAM
-wmic memorychip get serialnumber
-echo CPU
-wmic cpu get processorid
-echo BaseBoard
-wmic baseboard get manufacturer
-echo GPU
-wmic PATH Win32_VideoController GET Description,PNPDeviceID
-echo Mac Address
-getmac
-echo Other Stuff
-AMIDEWIN.EXE /dms serials.txt
-type serials.txt
-del /f serials.txt
-exit
 
 del output.txt /f1>nul 2>nul
 wmic diskdrive get serialnumber >output.txt
