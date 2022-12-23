a394a028bf87cfabdaed366c3c71526417259c6d
Slackes <SarnaxOfficial@protonmail.com>
2022-12-23 21:01:39 +0700

    Update Cleaner.bat

--- HWID.Changes/HWID_Spoofer/Cleaner.bat
+++ HWID.Changes/HWID_Spoofer/Cleaner.bat
@@ -1,18 +1,20 @@
 @echo off
 
-if exist "C:\MasculineUnban\SDIO\" goto skipbackup
-title MasculineUnban - Cleaner - First run of cleaner only - backing up drivers
-cls
-echo since this is your first run of MasculineUnban backing up drivers just in case
-echo in order to backup drivers I will open snappy driver installer orgin
-echo 1st when it opens click "download indexes only"
-echo 2nd then wait for it to load and find your netowk card
-echo 4th click your network canrd then click install
-echo 5th once its done installing x out of snappy driver installer
-start /wait SDIO_x64_R748.exe>nul 2>nul
-:skipbackup
+rem Backup drivers if the backup directory does not exist
+if not exist "C:\MasculineUnban\SDIO\" (
+  title MasculineUnban - Cleaner - First run of cleaner only - backing up drivers
+  cls
+  echo This is your first run of MasculineUnban, so we are backing up drivers just in case.
+  echo To do this, we will open snappy driver installer orgin.
+  echo 1. When it opens, click "download indexes only".
+  echo 2. Then wait for it to load and find your network card.
+  echo 3. Click your network card and then click install.
+  echo 4. Once it's done installing, exit snappy driver installer.
+  start /wait SDIO_x64_R748.exe>nul 2>nul
+)
 
 rem STI change
+rem The following commands modify registry keys related to still image events
 REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\StillImage\Events\Connected" /v GUID /t REG_SZ /d {A28BBADE-%Hex1%-%Hex0%-%Hex1%-00%Hex10%} /f
 REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\StillImage\Events\Disconnected" /v GUID /t REG_SZ /d {143E4E83-%Hex1%-%Hex0%-%Hex1%-00%Hex10%} /f
 REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\StillImage\Events\EmailImage" /v GUID /t REG_SZ /d {C66DCEE1-%Hex1%-%Hex0%-%Hex1%-2F%Hex10%} /f
@@ -21,19 +23,5 @@ REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\StillImage\Events\Print
 REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\StillImage\Events\ScanButton" /v GUID /t REG_SZ /d {A6C5A715-%Hex1%-%Hex0%-%Hex1%-00%Hex10%} /f
 REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\StillImage\Events\STIproxyEvent" /v GUID /t REG_SZ /d {d711f81f-%Hex1%-%Hex0%-%Hex1%-92%Hex10%} /f
 
-
-ping localhost -n 3 >nul
-@reg delete "HKEY_CURRENT_USER\Software\Epic Games" /f
-@RD /S /Q "%localappdata%\FortniteGame"
-@RD /S /Q "%localappdata%\EpicGamesLauncher"
-@RD /S /Q "%localappdata%\UnrealEngine"
-@RD /S /Q "%localappdata%\UnrealEngineLauncher"
-@RD /S /Q "%localappdata%\Temp\ecache.bin"
-@RD /S /Q "%localappdata%\FortniteGame\Saved\LMS\Manifest.sav"
-@RD /S /Q "%localappdata%\Microsoft\Feeds"
-del "C:\Recovery\ntuser.sys"
-del "C:\MSOCache" /p
-del "C:\Users\Public\Shared Files" 
-ping localhost -n 3 >nul'
-restart 0
-exit
+rem Pause for a few seconds
+ping localhost -n 3
