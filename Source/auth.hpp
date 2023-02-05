19a8d6a75f8004c93395ec03572e8e4d4edd6edd
Serups <SarnaxOfficial@protonmail.com>
2023-02-06 04:15:39 +0700

    C++ Class KeyAuth API

--- Source/auth.hpp
+++ Source/auth.hpp
@@ -13,17 +13,16 @@ namespace KeyAuth {
         void Check();
         void Log(const std::string& message);
         void License(const std::string& key);
-        std::string Var(const std::string& varId);
+        std::string GetVar(const std::string& varId);
         std::string Webhook(const std::string& id, const std::string& params);
-        void SetVar(const std::string& var, const std::string& varData);
-        std::string GetVar(const std::string& var);
-        bool CheckBlack();
+        void SetVar(const std::string& varId, const std::string& varData);
+        bool CheckBlacklist();
         void Upgrade(const std::string& username, const std::string& key);
         void Login(const std::string& username, const std::string& password);
         void WebLogin();
-        void Button(const std::string& value);
-        std::vector<unsigned char> Download(const std::string& fileId);
-        void Register(const std::string& username, const std::string& password, const std::string& key);
+        void SubmitButton(const std::string& buttonValue);
+        std::vector<unsigned char> DownloadFile(const std::string& fileId);
+        void RegisterUser(const std::string& username, const std::string& password, const std::string& key);
 
         struct Data {
             std::string numUsers;
@@ -32,12 +31,12 @@ namespace KeyAuth {
             std::string version;
             std::string customerPanelLink;
             std::string username;
-            std::string ip;
-            std::string hwid;
+            std::string ipAddress;
+            std::string hardwareId;
             std::string createdDate;
-            std::string lastLogin;
+            std::string lastLoginDate;
             std::vector<std::string> subscriptions;
-            std::string expiry;
+            std::string expiryDate;
             bool success;
             std::string message;
         };
