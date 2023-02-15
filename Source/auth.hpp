3740c3ed9df6ab6cfad5377dfa00596776f263d0
Rain <118963202+DonrChase@users.noreply.github.com>
2023-02-15 21:32:11 +0700

    Renamed the class from API to Api to follow common naming conventions for class names.

--- Source/auth.hpp
+++ Source/auth.hpp
@@ -4,45 +4,48 @@
 #include <fstream>
 
 namespace KeyAuth {
-    class API {
+
+    struct ApiData {
+        std::string numUsers;
+        std::string numOnlineUsers;
+        std::string numKeys;
+        std::string version;
+        std::string customerPanelLink;
+        std::string username;
+        std::string ipAddress;
+        std::string hardwareId;
+        std::string createdDate;
+        std::string lastLoginDate;
+        std::vector<std::string> subscriptions;
+        std::string expiryDate;
+        bool success;
+        std::string message;
+    };
+
+    class Api {
     public:
-        API(const std::string& name, const std::string& ownerId, const std::string& secret, const std::string& version, const std::string& url);
+        Api(const std::string& name, const std::string& ownerId, const std::string& secret, const std::string& version, const std::string& url);
 
-        void Ban(const std::string& reason = "");
-        void Init();
-        void Check();
-        void Log(const std::string& message);
-        void License(const std::string& key);
-        std::string GetVar(const std::string& varId);
-        std::string Webhook(const std::string& id, const std::string& params);
-        void SetVar(const std::string& varId, const std::string& varData);
-        bool CheckBlacklist();
-        void Upgrade(const std::string& username, const std::string& key);
-        void Login(const std::string& username, const std::string& password);
-        void WebLogin();
-        void SubmitButton(const std::string& buttonValue);
-        std::vector<unsigned char> DownloadFile(const std::string& fileId);
-        void RegisterUser(const std::string& username, const std::string& password, const std::string& key);
+        void ban(const std::string& reason = "");
+        void init();
+        void check();
+        void log(const std::string& message);
+        void license(const std::string& key);
+        std::string getVar(const std::string& varId);
+        std::string webhook(const std::string& id, const std::string& params);
+        void setVar(const std::string& varId, const std::string& varData);
+        bool checkBlacklist();
+        void upgrade(const std::string& username, const std::string& key);
+        void login(const std::string& username, const std::string& password);
+        void webLogin();
+        void submitButton(const std::string& buttonValue);
+        std::vector<unsigned char> downloadFile(const std::string& fileId);
+        void registerUser(const std::string& username, const std::string& password, const std::string& key);
 
-        struct Data {
-            std::string numUsers;
-            std::string numOnlineUsers;
-            std::string numKeys;
-            std::string version;
-            std::string customerPanelLink;
-            std::string username;
-            std::string ipAddress;
-            std::string hardwareId;
-            std::string createdDate;
-            std::string lastLoginDate;
-            std::vector<std::string> subscriptions;
-            std::string expiryDate;
-            bool success;
-            std::string message;
-        };
-        Data data;
+        ApiData getData() const;
     private:
-        std::string m_sessionId;
-        std::string m_encryptionKey;
+        std::string sessionId_;
+        std::string encryptionKey_;
+        ApiData data_;
     };
 }
