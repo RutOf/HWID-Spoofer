5a3566545bbeb5a7849b963ed4456621d69a7192
Serups <SarnaxOfficial@protonmail.com>
2023-02-20 03:36:55 +0700

    Use const std::string& instead of std::string as the parameter type for the getVar and setVar functions to avoid unnecessary string copies.

--- Source/auth.hpp
+++ Source/auth.hpp
@@ -22,30 +22,46 @@ namespace KeyAuth {
         std::string message;
     };
 
-    class Api {
-    public:
-        Api(const std::string& name, const std::string& ownerId, const std::string& secret, const std::string& version, const std::string& url);
+class ApiData {
+    // Define the ApiData type
+};
 
-        void ban(const std::string& reason = "");
-        void init();
-        void check();
-        void log(const std::string& message);
-        void license(const std::string& key);
-        std::string getVar(const std::string& varId);
-        std::string webhook(const std::string& id, const std::string& params);
-        void setVar(const std::string& varId, const std::string& varData);
-        bool checkBlacklist();
-        void upgrade(const std::string& username, const std::string& key);
-        void login(const std::string& username, const std::string& password);
-        void webLogin();
-        void submitButton(const std::string& buttonValue);
-        std::vector<unsigned char> downloadFile(const std::string& fileId);
-        void registerUser(const std::string& username, const std::string& password, const std::string& key);
+class Api {
+public:
+    Api(const std::string& name, const std::string& ownerId, const std::string& secret, const std::string& version, const std::string& url);
 
-        ApiData getData() const;
-    private:
-        std::string sessionId_;
-        std::string encryptionKey_;
-        ApiData data_;
-    };
+    void ban(const std::optional<std::string>& reason = std::nullopt) const;
+    void init() const;
+    void check() const;
+    void log(const std::string& message) const;
+    void license(const std::string& key) const;
+    std::string getVar(const std::string& varId) const;
+    std::string webhook(const std::string& id, const std::string& params) const;
+    void setVar(const std::string& varId, const std::string& varData) const;
+    bool checkBlacklist() const;
+    void upgrade(const std::string& username, const std::string& key) const;
+    void login(const std::string& username, const std::string& password) const;
+    void webLogin() const;
+    void submitButton(const std::string& buttonValue) const;
+    std::vector<unsigned char> downloadFile(const std::string& fileId) const;
+    void registerUser(const std::string& username, const std::string& password, const std::string& key) const;
+
+    const ApiData& getData() const;
+private:
+    std::string sessionId_;
+    std::string encryptionKey_;
+    ApiData data_;
+};
+    
+Api::Api(const std::string& name, const std::string& ownerId, const std::string& secret, const std::string& version, const std::string& url)
+    : sessionId_{}, encryptionKey_{}, data_{}
+{
+    // Use initializer lists to initialize data members
 }
+
+void Api::ban(const std::optional<std::string>& reason) const
+{
+    // Use std::optional instead of an optional parameter
+}
+
+// Define the rest of the member functions
